3f29a32603f61cc8ef4cecfc713f46c726ba52d0
Mufrat Karim Aritra <mufratkarimaritra@gmail.com>
2021-12-13 22:11:41 -0500

    "portfolio after windows setup"

--- app/src/main/java/com/mkalegend/android_portfolio/MainActivity.java
+++ app/src/main/java/com/mkalegend/android_portfolio/MainActivity.java
@@ -14,22 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RecyclerView recyclerViewProjects = findViewById(R.id.recyclerViewProjects);
-        Project[] projects = {
-                new Project("BMI Calculator",
-                        "A real life working BMI Calculator, using Variables, Methods and Conditional Logics.",
-                        R.drawable.bmi),
-                new Project("Dream Steak",
-                        "A menu app for a fictional restaurant, using Activities, Classes & Objects, Arrays, Intents and ListViews.",
-                        R.drawable.dream_steak),
-                new Project("Address Finder",
-                        "A multilingual app for retrieving the user address, using FireStore Database, Firebase Query Intents and Internationalization.",
-                        R.drawable.addressfind),
-                new Project("All Star Restaurants",
-                        "A review app of all the Firebase restaurants in realtime, using Application Lifecycle, Receivers, Intents and FireStore.",
-                        R.drawable.restaurant),
-        };
-
-        ProjectsAdapter adapter = new ProjectsAdapter(projects);
+        ProjectsAdapter adapter = new ProjectsAdapter(Project.projects);
         recyclerViewProjects.setAdapter(adapter);
 
     }
--- app/src/main/java/com/mkalegend/android_portfolio/Project.java
+++ app/src/main/java/com/mkalegend/android_portfolio/Project.java
@@ -10,4 +10,19 @@ public class Project {
         this.pDescription = pDescription;
         this.pImages = pImages;
     }
+
+    public static Project[] projects = {
+            new Project("BMI Calculator",
+                    "A real life working BMI Calculator, using Variables, Methods and Conditional Logics.",
+                    R.drawable.bmi),
+            new Project("Dream Steak",
+                    "A menu app for a fictional restaurant, using Activities, Classes & Objects, Arrays, Intents and ListViews.",
+                    R.drawable.dream_steak),
+            new Project("Self Marketing",
+                    "A self promoting app that helps a job hunter by messaging other contacts with a personalized text",
+                    R.drawable.addressfind),
+            new Project("Trip to BD",
+                    "A visually stunning app for all of my activities during my trip in Bangladesh.",
+                    R.drawable.restaurant)
+    };
 }
